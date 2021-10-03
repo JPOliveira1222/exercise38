e7cfb93c263cef431f4afbc8140ae8af03cf2164
JP Oliveira <joaopedro.costa.oliveira@gmail.com>
2021-10-03 18:09:44 -0400

    Pseudocode

---
+++ src/main/java/exercise38/solution38.java
@@ -0,0 +1,32 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 38 Solution
+ *  Copyright 2021 Joao Pedro Oliveira
+ */
+
+package exercise38;
+
+public class solution38 {
+
+    /*
+    Prompt the user to enter a string containing numbers separated by the space
+    Print "Enter a list of numbers, separated by spaces: "
+
+    Have a function that returns the integer array of all the even numbers in the List
+
+    print "The even numbers are "
+
+    display the list of only even numbers using a for loop
+    using an if/else loop keep going through the numbers until there are no more even numbers in the list
+
+    the method to go through the numbers and check if they are even
+
+    make an empty Integer array list
+    loop to go through the inputs
+    convert the string to integers
+    check to see if the numbers are divisible by 2, if they are, add them to the empty list.
+
+
+
+
+     */
+}
